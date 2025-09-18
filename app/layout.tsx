7001e2eec4fb398c0483b307841b47455b186d4c
pierre <pierre.boisnard@live.fr>
2025-09-18 14:36:46 +0200

    remove .html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
-import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  themeColor: "#3b82f6",
-};
 
 export const metadata: Metadata = {
   title: "Pierre Boisnard - Développeur Full-Stack",
@@ -18,6 +12,12 @@ export const metadata: Metadata = {
     "développeur, full-stack, React, Next.js, Node.js, MongoDB, freelance, portfolio",
   authors: [{ name: "Pierre Boisnard" }],
   creator: "Pierre Boisnard",
+  themeColor: "#3b82f6",
+  icons: {
+    icon: "/icons/favicon-32x32.png",
+    apple: "/icons/apple-touch-icon.png",
+    shortcut: "/icons/favicon-32x32.png",
+  },
   openGraph: {
     title: "Pierre Boisnard - Développeur Full-Stack",
     description:
@@ -25,16 +25,30 @@ export const metadata: Metadata = {
     url: "https://portfolio-frontend-neon-six.vercel.app/",
     siteName: "Pierre Boisnard Portfolio",
     type: "website",
+    images: [
+      {
+        url: "/images/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Logo Pierre Boisnard",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pierre Boisnard - Développeur Full-Stack",
+    description:
+      "Portfolio de Pierre Boisnard, développeur full-stack spécialisé en React, Next.js, Node.js et MongoDB.",
+    images: ["/images/og-image.jpg"],
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
